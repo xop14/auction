60769e4964b10c4a57c5c2a48b835a95a6a85e4b
xop14 <112329100+xop14@users.noreply.github.com>
2022-11-29 22:50:04 +0900

    Add create listing page

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,45 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    category = models.CharField(max_length=32)
+    photo_url = models.URLField(blank=True)
+    is_active = models.BooleanField(default=True)
+    starting_bid = models.DecimalField(decimal_places=2, max_digits=7)
+    user_id = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"ID: {self.id}, \nTitle: {self.title}, \nCategory: {self.category}, \nDescription: {self.description}, \nPhoto URL: {self.photo_url}, \nActive: {self.is_active}, \nStarting Bid: {self.starting_bid}, \nCreated: {self.created}, \nUpdated: {self.updated}"
+
+class Bid(models.Model): 
+    time_stamp = models.DateTimeField(auto_now=True)
+    listing_id = models.IntegerField()
+    user_id = models.IntegerField()
+    bid_amount = models.DecimalField(decimal_places=2, max_digits=7)
+    
+    def __str__(self):
+        return f"""
+                TimeStamp: {self.time_stamp}
+                Listing ID: {self.listing_id}
+                User ID: {self.user_id}
+                Bid Amount: {self.bid_amount}
+                """
+
+class Comment(models.Model):
+    time_stamp = models.DateTimeField(auto_now=True)
+    listing_id = models.IntegerField()
+    user_id = models.IntegerField()
+    comment = models.CharField(max_length=1024)
+    
+    def __str__(self):
+        return f"""
+                TimeStamp: {self.time_stamp}
+                Listing ID: {self.listing_id}
+                User ID: {self.user_id}
+                Comment: {self.comment}
+                """
--- auctions/views.py
+++ auctions/views.py
@@ -1,15 +1,64 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.core.exceptions import ObjectDoesNotExist
+from django import forms
 
 from .models import User
+from .models import Listing
+from .models import Bid
+from .models import Comment
 
+class CreateListingForm(forms.Form):
+    title = forms.CharField(label="Title", max_length=64)
+    description = forms.CharField(label="Description", widget=forms.Textarea(attrs={'cols':10, 'rows':3}), max_length=1024)
+    category = forms.CharField(label="Category", max_length=32)
+    photo_url = forms.URLField(label="Photo URL (optional)", required=False)
+    starting_bid = forms.DecimalField(label="Starting bid (USD)", decimal_places=2, max_digits=7)
 
 def index(request):
-    return render(request, "auctions/index.html")
+    print(request.user.username)
+    return render(request, "auctions/index.html", {
+        "listings": Listing.objects.all(),
+        "current_user_id": request.user.id,
+    })
+    
+def create(request):
+    
+    if request.method == "POST":
+        form = CreateListingForm(request.POST)
+        if form.is_valid():
+            listing = Listing(
+                title = form.cleaned_data["title"],
+                description = form.cleaned_data["description"],
+                category = form.cleaned_data["category"],
+                photo_url = form.cleaned_data["photo_url"],
+                starting_bid = form.cleaned_data["starting_bid"],
+                user_id = request.user.id
+            )
+            listing.save()
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "auctions/create.html", {
+            "form": CreateListingForm(),
+        })
 
+def listing(request, listing_id):
+    error_message = None
+    
+    try:
+        listing = Listing.objects.get(id=listing_id)
+    except ObjectDoesNotExist:
+        error_message = "This listing does not exist"
+        listing = None
+    
+    return render(request, "auctions/listing.html", {
+        "listing": listing,
+        "error_message": error_message
+    })
 
 def login_view(request):
     if request.method == "POST":
