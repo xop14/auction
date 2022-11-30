8da24dd5a65e568d81b724b76612b7b0e5c07fbe
xop14 <112329100+xop14@users.noreply.github.com>
2022-11-30 19:55:54 +0900

    Add bid min limit

--- auctions/views.py
+++ auctions/views.py
@@ -12,15 +12,20 @@ from .models import Listing
 from .models import Bid
 from .models import Comment
 
+
+### forms ###
+
 class CreateListingForm(forms.Form):
     title = forms.CharField(label="Title", max_length=64)
     description = forms.CharField(label="Description", widget=forms.Textarea(attrs={'cols':10, 'rows':3}), max_length=1024)
     category = forms.CharField(label="Category", max_length=32)
     photo_url = forms.URLField(label="Photo URL (optional)", required=False)
     starting_bid = forms.DecimalField(label="Starting bid (USD)", decimal_places=2, max_digits=7)
+    
+    
+    
 
-class BidForm(forms.Form):
-    bid_amount = forms.DecimalField(decimal_places=2, max_digits=7)
+### main pages ###
 
 def index(request):
     return render(request, "auctions/index.html", {
@@ -28,7 +33,9 @@ def index(request):
         "current_user_id": request.user.id
     })
 
-@login_required    
+
+
+@login_required()
 def create(request):
     
     if request.method == "POST":
@@ -49,65 +56,9 @@ def create(request):
             "form": CreateListingForm(),
         })
 
-def listing(request, listing_id):
-    error_message = None
-    if Bid.objects.filter(listing_id = listing_id):
-        current_highest_bid = Bid.objects.filter(listing_id = listing_id).order_by('-bid_amount')[0].bid_amount
-    else:
-        current_highest_bid = "No bids yet"
-    
-    
-    if request.method == "POST":
-        print(request.POST)
-        this_listing = Listing.objects.get(pk=listing_id)
-        
-        # button actions for delete, edit, end
-        if "delete" in request.POST:
-            this_listing.delete()
-            return HttpResponseRedirect(reverse("index"))
-        if "edit" in request.POST:
-            return HttpResponseRedirect(reverse("edit", kwargs={"listing_id": listing_id}))
-        if "end" in request.POST:
-            this_listing.is_active = False
-            this_listing.save()
-            
-        # bid section
-        if "bid_amount" in request.POST:
-            this_bid = request.POST["bid_amount"]
-            bid = Bid(
-                user_id = request.user.id,
-                listing_id = listing_id,
-                bid_amount = request.POST["bid_amount"]
-                )
-            bid.save()
-            return HttpResponseRedirect(reverse("listing", kwargs={"listing_id": listing_id}))
-            
-        
-    # get listing content
-    try:
-        listing = Listing.objects.get(id=listing_id)
-    except ObjectDoesNotExist:
-        error_message = "This listing does not exist"
-        listing = None
-    
-    # get listing creators username
-    try:
-        creator = User.objects.get(id=listing.user_id)
-    except ObjectDoesNotExist:
-        error_message = "This user no longer exists"
-        creator = None
-    
-    bid_form = BidForm()
-    
-    return render(request, "auctions/listing.html", {
-        "listing": listing,
-        "error_message": error_message,
-        "creator": creator,
-        "bid_form": bid_form,
-        "current_highest_bid": current_highest_bid
-    })
 
 
+@login_required()
 def edit(request, listing_id):
     
     # get listing content
@@ -148,6 +99,84 @@ def edit(request, listing_id):
     })
 
 
+
+def listing(request, listing_id):
+    
+    error_message = None
+    
+    # check if bids exist for this listing and set bid-related variables
+    if Bid.objects.filter(listing_id = listing_id):
+        current_highest_bid = Bid.objects.filter(listing_id = listing_id).order_by('-bid_amount')[0].bid_amount
+        bidder_id = Bid.objects.filter(listing_id = listing_id).order_by('-bid_amount')[0].user_id
+        bidder_username = User.objects.get(id = bidder_id).username   
+    else:
+        current_highest_bid = 0.00
+        bidder_id = None
+        bidder_username = None
+    
+    # get listing content
+    try:
+        listing = Listing.objects.get(id=listing_id)
+    except ObjectDoesNotExist:
+        error_message = "This listing does not exist"
+        listing = None
+    
+    
+    # post
+    if request.method == "POST":
+        print(request.POST)
+        
+        # button actions for delete, edit, end
+        if "delete" in request.POST:
+            listing.delete()
+            return HttpResponseRedirect(reverse("index"))
+        if "edit" in request.POST:
+            return HttpResponseRedirect(reverse("edit", kwargs={"listing_id": listing_id}))
+        if "end" in request.POST:
+            listing.is_active = False
+            listing.save()
+            
+        # bid section
+        if "bid_amount" in request.POST:
+            this_bid = float(request.POST["bid_amount"])
+            bid = Bid(
+                user_id = request.user.id,
+                listing_id = listing_id,
+                bid_amount = this_bid
+                )
+            bid.save()
+            return HttpResponseRedirect(reverse("listing", kwargs={"listing_id": listing_id}))
+            
+    
+    # get listing creators username
+    try:
+        creator = User.objects.get(id=listing.user_id)
+    except ObjectDoesNotExist:
+        error_message = "This user no longer exists"
+        creator = None
+    
+    # create form within this view to is has access to this views variables for easier validation
+    class BidForm(forms.Form):
+        bid_amount = forms.DecimalField(decimal_places=2, max_digits=7, min_value=max(current_highest_bid, listing.starting_bid) + 1)
+    
+    bid_form = BidForm()
+    
+    return render(request, "auctions/listing.html", {
+        "listing": listing,
+        "error_message": error_message,
+        "creator": creator,
+        "bid_form": bid_form,
+        "current_highest_bid": current_highest_bid,
+        "bidder_username": bidder_username
+    })
+
+
+
+
+
+
+
+### user management ###
 
 def login_view(request):
     if request.method == "POST":
