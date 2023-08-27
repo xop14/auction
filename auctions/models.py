f781b3cc8394d2eda6727c89685fe1df5d78f097
David Green <d-green@grune.co.jp>
2023-08-27 16:16:06 +0900

    final version

--- auctions/models.py
+++ auctions/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+
 class Listing(models.Model):
     title = models.CharField(max_length=64)
     description = models.TextField()
@@ -12,35 +13,44 @@ class Listing(models.Model):
     photo_url = models.URLField(blank=True)
     is_active = models.BooleanField(default=True)
     starting_bid = models.DecimalField(decimal_places=2, max_digits=10)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="listings")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return f"{self.id} | {self.title} | {self.category}"
 
-class Bid(models.Model): 
+
+class Bid(models.Model):
     time_stamp = models.DateTimeField(auto_now=True)
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    listing = models.ForeignKey(
+        Listing, on_delete=models.CASCADE, related_name="bids")
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="bids")
     bid_amount = models.DecimalField(decimal_places=2, max_digits=7)
-    
+
     def __str__(self):
         return f"{self.user} | ${self.bid_amount} | listing {self.listing_id}"
 
+
 class Comment(models.Model):
     time_stamp = models.DateTimeField(auto_now=True)
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    listing = models.ForeignKey(
+        Listing, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="comments")
     comment = models.CharField(max_length=1024)
-    
+
     def __str__(self):
         return f"{self.user} | '{self.comment}' | listing {self.listing_id}"
 
+
 class Watchlist(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="watchlist")
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="watchlist")
     listings = models.ManyToManyField(Listing)
     updated = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return f"{self.user}"
--- auctions/views.py
+++ auctions/views.py
@@ -12,27 +12,29 @@ from decimal import Decimal
 from .models import *
 
 
-
-
 ### FORMS ###
 
 class CreateListingForm(forms.Form):
     title = forms.CharField(label="Title", max_length=64)
-    description = forms.CharField(label="Description (max. 1024 characters)", widget=forms.Textarea(attrs={'cols':10, 'rows':8}), max_length=1024)
+    description = forms.CharField(label="Description (max. 1024 characters)", widget=forms.Textarea(
+        attrs={'cols': 10, 'rows': 8}), max_length=1024)
     category = forms.CharField(label="Category", max_length=32)
     photo_url = forms.URLField(label="Photo URL (optional)", required=False)
-    starting_bid = forms.DecimalField(label="Starting bid (USD)", decimal_places=2, max_digits=7, max_value=9999.99, widget=forms.TextInput(attrs={'id': 'bid-field'}))
-    
+    starting_bid = forms.DecimalField(label="Starting bid (USD)", decimal_places=2,
+                                      max_digits=7, max_value=9999.99, widget=forms.TextInput(attrs={'id': 'bid-field'}))
+
+
 class EditListingForm(forms.Form):
     title = forms.CharField(label="Title", max_length=64)
-    description = forms.CharField(label="Description (max. 1024 characters)", widget=forms.Textarea(attrs={'cols':10, 'rows':8}), max_length=1024)
+    description = forms.CharField(label="Description (max. 1024 characters)", widget=forms.Textarea(
+        attrs={'cols': 10, 'rows': 8}), max_length=1024)
     category = forms.CharField(label="Category", max_length=32)
     photo_url = forms.URLField(label="Photo URL (optional)", required=False)
 
+
 class CommentForm(forms.Form):
-    comment = forms.CharField(label="Leave comment", widget=forms.Textarea(attrs={'cols':10, 'rows':3}), max_length=1024)
-
-
+    comment = forms.CharField(label="Leave comment", widget=forms.Textarea(
+        attrs={'cols': 10, 'rows': 3}), max_length=1024)
 
 
 ### FUNCTIONS ###
@@ -40,48 +42,47 @@ class CommentForm(forms.Form):
 # adds highest bid, bidder and the bid count to a listings object
 def add_bidinfo(listings):
     for listing in listings:
-        if Bid.objects.filter(listing = listing.id):
-            listing.bid_count = Bid.objects.filter(listing = listing.id).count()
-            listing.highest_bid = Bid.objects.filter(listing = listing.id).order_by('-bid_amount')[0].bid_amount
-            listing.highest_bidder = Bid.objects.filter(listing = listing.id).order_by('-bid_amount')[0].user
+        if Bid.objects.filter(listing=listing.id):
+            listing.bid_count = Bid.objects.filter(listing=listing.id).count()
+            listing.highest_bid = Bid.objects.filter(
+                listing=listing.id).order_by('-bid_amount')[0].bid_amount
+            listing.highest_bidder = Bid.objects.filter(
+                listing=listing.id).order_by('-bid_amount')[0].user
         else:
             listing.bid_count = None
             listing.highest_bid = None
             listing.highest_bidder = None
-    
+
     return listings
-    
-    
 
 
 ### MAIN PAGES ###
 
 def index(request):
-    
+
     # get list of active listings
-    listings = Listing.objects.filter(is_active = True)
+    listings = Listing.objects.filter(is_active=True)
     add_bidinfo(listings)
-    
+
     return render(request, "auctions/index.html", {
         "listings": listings,
         "title": "Active Listings",
     })
 
 
-
 @login_required()
 def create(request):
-    
+
     if request.method == "POST":
         form = CreateListingForm(request.POST)
         if form.is_valid():
             listing = Listing(
-                title = form.cleaned_data["title"],
-                description = form.cleaned_data["description"],
-                category = form.cleaned_data["category"].casefold(),
-                photo_url = form.cleaned_data["photo_url"],
-                starting_bid = form.cleaned_data["starting_bid"],
-                user = request.user
+                title=form.cleaned_data["title"],
+                description=form.cleaned_data["description"],
+                category=form.cleaned_data["category"].casefold(),
+                photo_url=form.cleaned_data["photo_url"],
+                starting_bid=form.cleaned_data["starting_bid"],
+                user=request.user
             )
             listing.save()
         return HttpResponseRedirect(reverse("index"))
@@ -91,10 +92,9 @@ def create(request):
         })
 
 
-
 @login_required()
 def edit(request, listing_id):
-    
+
     # get listing content
     try:
         listing = Listing.objects.get(id=listing_id)
@@ -102,8 +102,7 @@ def edit(request, listing_id):
         return render(request, "auctions/edit.html", {
             "error_message": "No listing found"
         })
-    
-    
+
     if request.method == "POST":
         form = EditListingForm(request.POST)
         if form.is_valid():
@@ -113,7 +112,6 @@ def edit(request, listing_id):
             listing.photo_url = form.cleaned_data["photo_url"]
             listing.save()
         return HttpResponseRedirect(reverse("listing", kwargs={"listing_id": listing_id}))
-    
 
     # create form pre-populated with listing's data
     form = EditListingForm({
@@ -121,8 +119,8 @@ def edit(request, listing_id):
         "description": listing.description,
         "category": listing.category,
         "photo_url": listing.photo_url,
-        })
-    
+    })
+
     return render(request, "auctions/edit.html", {
         "listing_id": listing_id,
         "listing": listing,
@@ -130,27 +128,28 @@ def edit(request, listing_id):
     })
 
 
-
 def listing(request, listing_id):
-    
+
     # check if bids exist for this listing and set bid-related variables
-    if Bid.objects.filter(listing = listing_id):
-        current_highest_bid = Bid.objects.filter(listing = listing_id).order_by('-bid_amount')[0].bid_amount
-        current_highest_bidder = Bid.objects.filter(listing = listing_id).order_by('-bid_amount')[0].user
+    if Bid.objects.filter(listing=listing_id):
+        current_highest_bid = Bid.objects.filter(
+            listing=listing_id).order_by('-bid_amount')[0].bid_amount
+        current_highest_bidder = Bid.objects.filter(
+            listing=listing_id).order_by('-bid_amount')[0].user
     else:
         current_highest_bid = None
         current_highest_bidder = None
-    
+
     # get listing content and return error message if listing does not exist
     try:
         listing = Listing.objects.get(id=listing_id)
     except ObjectDoesNotExist:
         return render(request, "auctions/listing.html", {
-        "error_message": "No listing found"
-    })
-    
+            "error_message": "No listing found"
+        })
+
     # get watchlist status
-    # get_or_create returns a tuple and so requires the 'created' part which returns True if created and False if not
+    # or_create returns a tuple and so requires the 'created' part which returns True if created and False if not
     try:
         watchlist, created = Watchlist.objects.get_or_create(user=request.user)
         if listing in watchlist.listings.all():
@@ -159,47 +158,47 @@ def listing(request, listing_id):
             is_watchlist = False
     except TypeError:
         is_watchlist = False
-        
+
     # get comments
     try:
-        comments = Comment.objects.filter(listing = listing_id).all()
+        comments = Comment.objects.filter(listing=listing_id).all()
     except ObjectDoesNotExist:
         comments = None
-        
+
     # add comment form
     comment_form = CommentForm()
-    
+
     # calculate minimum bid for form validation min value
     if current_highest_bid:
         min_bid = round(current_highest_bid + Decimal(0.01), 2)
-    else: 
+    else:
         min_bid = round(listing.starting_bid + Decimal(0.01), 2)
-    
+
     # create form within this view to is has access to this views variables for easier client-side validation
     class BidForm(forms.Form):
-        bid_amount = forms.DecimalField(label="Bid amount", decimal_places=2, max_digits=7, min_value = min_bid, max_value=10000, widget=forms.NumberInput(attrs={'placeholder': f"{min_bid:.2f} or more", 'id': 'bid-field'}))
-    
+        bid_amount = forms.DecimalField(label="Bid amount", decimal_places=2, max_digits=7, min_value=min_bid,
+                                        max_value=10000, widget=forms.NumberInput(attrs={'placeholder': f"{min_bid:.2f} or more", 'id': 'bid-field'}))
+
     bid_form = BidForm()
     bid_error = None
     this_bid = None
-    
-    
-    # POST 
+
+    # POST
     if request.method == "POST":
         print(request.POST)
-        
+
         # button actions for delete, edit, end, watchlist
         if "delete" in request.POST:
             listing.delete()
             return HttpResponseRedirect(reverse("index"))
-        
+
         if "edit" in request.POST:
             return HttpResponseRedirect(reverse("edit", kwargs={"listing_id": listing_id}))
-        
+
         if "end" in request.POST:
             listing.is_active = False
             listing.save()
-            
+
         if "toggle-watchlist" in request.POST:
             # add or remove toggle from watchlist
             if listing in watchlist.listings.all():
@@ -209,39 +208,40 @@ def listing(request, listing_id):
                 watchlist.listings.add(listing)
                 is_watchlist = True
             watchlist.save()
-            
+
         # bid section
         if "bid_amount" in request.POST:
             bid_form = BidForm(request.POST)
-            
+
             if bid_form.is_valid():
                 this_bid = bid_form.cleaned_data["bid_amount"]
                 bid = Bid(
-                    user_id = request.user.id,
-                    listing_id = listing_id,
-                    bid_amount = this_bid
-                    )
+                    user_id=request.user.id,
+                    listing_id=listing_id,
+                    bid_amount=this_bid
+                )
                 bid.save()
                 return HttpResponseRedirect(reverse("listing", kwargs={"listing_id": listing_id}))
             else:
                 this_bid = None
                 # create the form already instantiated with the submitted bid data
-                bid_form = BidForm(initial={"bid_amount": request.POST["bid_amount"]})
+                bid_form = BidForm(
+                    initial={"bid_amount": request.POST["bid_amount"]})
                 bid_error = "Please enter a valid bid"
 
         # comment section
         if "comment" in request.POST:
             comment_form = CommentForm(request.POST)
-            
+
             if comment_form.is_valid():
                 comment = Comment(
-                    comment = comment_form.cleaned_data["comment"],
-                    listing = listing,
-                    user = request.user
-                    )
+                    comment=comment_form.cleaned_data["comment"],
+                    listing=listing,
+                    user=request.user
+                )
                 comment.save()
                 return HttpResponseRedirect(reverse("listing", kwargs={"listing_id": listing_id}))
-                
+
     # return the listings page
     return render(request, "auctions/listing.html", {
         "listing": listing,
@@ -256,46 +256,43 @@ def listing(request, listing_id):
     })
 
 
-
 def categories(request):
-    
-    listings = Listing.objects.filter(is_active = True).order_by("category")
-    
+
+    listings = Listing.objects.filter(is_active=True).order_by("category")
+
     categories = []
-    
+
     for listing in listings:
         if listing.category not in categories:
             categories.append(listing.category)
-        
+
     return render(request, "auctions/categories.html", {
         "categories": categories,
     })
 
 
-
 def category(request, category_name):
-    
-    listings = Listing.objects.filter(is_active = True).filter(category = category_name)
+
+    listings = Listing.objects.filter(
+        is_active=True).filter(category=category_name)
     add_bidinfo(listings)
-        
+
     return render(request, "auctions/index.html", {
         "listings": listings,
         "title": f"Category: {category_name.title()}",
     })
 
 
-
 def watchlist(request):
-    
+
     try:
-        watchlist = Watchlist.objects.get(user = request.user)
+        watchlist = Watchlist.objects.get(user=request.user)
         watchlist_items = watchlist.listings.order_by("-is_active").all()
     except ObjectDoesNotExist:
         watchlist_items = None
-    
+
     add_bidinfo(watchlist_items)
-    
-    
+
     # remove listing from watchlist
     if request.method == "POST":
         if "remove_from_watchlist" in request.POST:
@@ -304,61 +301,62 @@ def watchlist(request):
             watchlist.listings.remove(listing)
             watchlist.save()
             watchlist_items = watchlist.listings.all()
-        
+
     return render(request, "auctions/index.html", {
         "listings": watchlist_items,
         "title": "My Watchlist",
     })
 
 
-
 def mylistings(request):
-    
+
     # get list of active listings
-    listings = Listing.objects.filter(user = request.user).order_by("-is_active")
-    
+    listings = Listing.objects.filter(user=request.user).order_by("-is_active")
+
     # get highest bid for each listing from Bid table
     for listing in listings:
-        if Bid.objects.filter(listing = listing.id):
-            listing.bid_count = Bid.objects.filter(listing = listing.id).count()
-            listing.highest_bid = Bid.objects.filter(listing = listing.id).order_by('-bid_amount')[0].bid_amount
-            listing.highest_bidder = Bid.objects.filter(listing = listing.id).order_by('-bid_amount')[0].user
+        if Bid.objects.filter(listing=listing.id):
+            listing.bid_count = Bid.objects.filter(listing=listing.id).count()
+            listing.highest_bid = Bid.objects.filter(
+                listing=listing.id).order_by('-bid_amount')[0].bid_amount
+            listing.highest_bidder = Bid.objects.filter(
+                listing=listing.id).order_by('-bid_amount')[0].user
         else:
             listing.highest_bid = None
-    
+
     return render(request, "auctions/index.html", {
         "listings": listings,
         "title": "My Listings",
     })
 
 
-
 def mybids(request):
-    
+
     # get list of active listings which the current user has bid on
-    listings = Listing.objects.filter(bids__user = request.user).order_by("-is_active")
+    listings = Listing.objects.filter(
+        bids__user=request.user).order_by("-is_active")
     bid_listings = []
-    
+
     for listing in listings:
         if listing not in bid_listings:
             bid_listings.append(listing)
     print(bid_listings)
-    
+
     # get highest bid and bidder for each listing from Bid table
     for listing in listings:
-        if Bid.objects.filter(listing = listing.id):
-            listing.bid_count = Bid.objects.filter(listing = listing.id).count()
-            listing.highest_bid = Bid.objects.filter(listing = listing.id).order_by('-bid_amount')[0].bid_amount
-            listing.highest_bidder = Bid.objects.filter(listing = listing.id).order_by('-bid_amount')[0].user
+        if Bid.objects.filter(listing=listing.id):
+            listing.bid_count = Bid.objects.filter(listing=listing.id).count()
+            listing.highest_bid = Bid.objects.filter(
+                listing=listing.id).order_by('-bid_amount')[0].bid_amount
+            listing.highest_bidder = Bid.objects.filter(
+                listing=listing.id).order_by('-bid_amount')[0].user
         else:
             listing.highest_bid = None
-    
+
     return render(request, "auctions/index.html", {
         "listings": bid_listings,
         "title": "My Bids",
     })
-
-
 
 
 ### USER MANAGEMENT (Pre-made by CS50W staff) ###
